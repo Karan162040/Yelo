4d0c035d49e3d56913f9bfb8ccfa639bb8217f35
Karan162040 <kumar.karan.162040@gmail.com>
2024-05-15 03:19:23 +0530

    Added Few Styles

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,14 @@
 import React from "react";
 import Box from "./Box";
+import "./Board.css";
 
 const Board = (props) => {
   return (
-    <>
+    <div className="papu">
       {props.Data.map((item) => {
         return <Box key={item.title} Data={item} />;
       })}
-    </>
+    </div>
   );
 };
 
--- src/components/Box.js
+++ src/components/Box.js
@@ -1,10 +1,12 @@
 import React from "react";
+import "./Box.css";
 
 const Box = (props) => {
   return (
-    <div>
+    <div className="outline">
       <h2>{props.Data.title}</h2>
       <h5>{props.Data.des}</h5>
+
       <button>Add</button>
       <button>Remove</button>
     </div>
